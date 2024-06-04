15e926acdd1df26558d622b8b9105422d7443c9e
Max Ortner <mortner@lanl.gov>
2024-06-03 23:53:14 -0600

    test commit

---
+++ test.py
@@ -0,0 +1,11 @@
+import unittest
+
+class TestMethods(unittest.TestCase):
+    def test_a(self):
+        self.assertEqual(2 + 2, 4)
+
+    def test_b(self):
+        self.assertFalse(2 + 2 == 3)
+
+if __name__ == '__main__':
+    unittest.main()
